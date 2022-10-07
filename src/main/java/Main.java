1d34c1af19b815a22fc88c276980d4f47db5f977
bozhokmaria <bozhokmaria@gmail.com>
2022-10-07 16:04:21 +0300

    small add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LOGGER.info("Navigator initial app");
     }
 }
