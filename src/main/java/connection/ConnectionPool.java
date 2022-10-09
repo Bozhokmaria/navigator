ec773392c473bf7cacca15d12735d82cbcee62dd
Dima Shevtsov <shevadima15z@gmail.com>
2022-10-10 00:00:25 +0300

    Added connection

---
+++ src/main/java/connection/ConnectionPool.java
@@ -0,0 +1,72 @@
+package connection;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class ConnectionPool {
+    private final static Logger log = LogManager.getLogger(ConnectionPool.class);
+
+    public static Connection getConnection() {
+        Connection c = null;
+
+        try (FileInputStream f = new FileInputStream("src/main/resources/db.properties")) {
+
+            Properties properties = new Properties();
+            properties.load(f);
+
+            String url = properties.getProperty("db.url");
+            String user = properties.getProperty("db.user");
+            String password = properties.getProperty("db.password");
+
+            c = DriverManager.getConnection(url, user, password);
+
+            if (c != null) {
+                log.info("Connected");
+            }
+        } catch (SQLException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return c;
+    }
+
+    public static void closeConnection(Connection c) {
+        try {
+            if (c != null) {
+                c.close();
+                log.info("Connection closed");
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closePreparedStatement(PreparedStatement ps) {
+        try {
+            if (ps != null) {
+                ps.close();
+                log.info("PreparedStatement closed");
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+                log.info("ResultSet closed");
+            }
+        }
+        catch (SQLException se) {
+            se.printStackTrace();
+        }
+    }
+}
