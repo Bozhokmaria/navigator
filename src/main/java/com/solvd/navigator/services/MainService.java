c15fae4babfd75ac1e8baf9aa7bba61ce695a92b
Gleb Chekmezov <gleeebkachek@gmail.com>
2022-10-12 12:34:32 +0300

    refactoring

--- src/main/java/com/solvd/navigator/services/MainService.java
+++ src/main/java/com/solvd/navigator/services/MainService.java
@@ -14,39 +14,11 @@ public class MainService {
 
     public static void task() {
         List<String> cityNames = CityService.getAndShowAllCitiesNames();
-        // get and show all cities
         int[] citiesFromAndTo = CityService.getCityFromAndCityTo(cityNames); // [0] - from, [1] - to
-        // get cityFrom and cityTo
-        NodeService.Node[] nodes = new NodeService.Node[cityNames.size()];
-        for(int i = 0; i < cityNames.size(); i++) {
-            nodes[i] = new NodeService.Node(cityNames.get(i));
-        }
+        NodeService.Node[] nodes = NodeService.getNodes(cityNames);
         List<List<Distance>> distancesForNodes = DistanceService.getDistancesForEveryNode(cityNames);
-
-        for(int i = 0; i < nodes.length; i++) {
-            for (int j = 0; j < distancesForNodes.get(i).size(); j++) {
-                if (distancesForNodes.get(i).get(j).getCity1_id() == (i + 1)) {
-                    int tmpId2 = distancesForNodes.get(i).get(j).getCity2_id();
-                    nodes[i].addDestination(nodes[tmpId2 - 1], distancesForNodes.get(i).get(j).getDistance());
-
-                } else if (distancesForNodes.get(i).get(j).getCity2_id() == (i + 1)) {
-                    int tmpId1 = distancesForNodes.get(i).get(j).getCity1_id();
-                    nodes[i].addDestination(nodes[tmpId1 - 1], distancesForNodes.get(i).get(j).getDistance());
-                }
-            }
-        }
-
-        GraphService.Graph graph = new GraphService.Graph();
-        for(int i = 0; i < nodes.length; i++) {
-            graph.addNode(nodes[i]);
-        }
-        graph = GraphService.calculateShortestPathFromSource(graph, nodes[citiesFromAndTo[0]]);
-        NodeService.Node[] resultPath = new NodeService.Node[nodes[citiesFromAndTo[1]].getShortestPath().size() + 1];
-        for(int i = 0; i < resultPath.length - 1; i++) {
-            resultPath[i] = nodes[citiesFromAndTo[1]].getShortestPath().get(i);
-        }
-        resultPath[resultPath.length - 1] = nodes[citiesFromAndTo[1]];
-
+        NodeService.addAllRelatedNodesToNode(nodes, distancesForNodes);
+        NodeService.Node[] resultPath = NodeService.getResultPath(nodes, citiesFromAndTo);
         LOGGER.info(Arrays.toString(resultPath) + " ; Distance -> " + resultPath[resultPath.length - 1].getDistance());
 
     }
--- src/main/java/com/solvd/navigator/services/NodeService.java
+++ src/main/java/com/solvd/navigator/services/NodeService.java
@@ -1,5 +1,7 @@
 package com.solvd.navigator.services;
 
+import com.solvd.navigator.model.Distance;
+
 import java.util.*;
 
 public class NodeService {
@@ -85,6 +87,43 @@ public class NodeService {
         return lowestDistanceNode;
     }
 
+    public static Node[] getNodes(List<String> cityNames) {
+        NodeService.Node[] nodes = new NodeService.Node[cityNames.size()];
+        for(int i = 0; i < cityNames.size(); i++) {
+            nodes[i] = new NodeService.Node(cityNames.get(i));
+        }
+        return nodes;
+    }
 
+    public static void addAllRelatedNodesToNode(Node[] nodes, List<List<Distance>> distancesForNodes) {
+        for(int i = 0; i < nodes.length; i++) {
+            for (int j = 0; j < distancesForNodes.get(i).size(); j++) {
+                if (distancesForNodes.get(i).get(j).getCity1_id() == (i + 1)) {
+                    int tmpId2 = distancesForNodes.get(i).get(j).getCity2_id();
+                    nodes[i].addDestination(nodes[tmpId2 - 1], distancesForNodes.get(i).get(j).getDistance());
+
+                } else if (distancesForNodes.get(i).get(j).getCity2_id() == (i + 1)) {
+                    int tmpId1 = distancesForNodes.get(i).get(j).getCity1_id();
+                    nodes[i].addDestination(nodes[tmpId1 - 1], distancesForNodes.get(i).get(j).getDistance());
+                }
+            }
+        }
+    }
+
+    public static Node[] getResultPath(Node[] nodes, int[] citiesFromAndTo) {
+        GraphService.Graph graph = new GraphService.Graph();
+        for(int i = 0; i < nodes.length; i++) {
+            graph.addNode(nodes[i]);
+        }
+
+        graph = GraphService.calculateShortestPathFromSource(graph, nodes[citiesFromAndTo[0]]);
+        NodeService.Node[] resultPath = new NodeService.Node[nodes[citiesFromAndTo[1]].getShortestPath().size() + 1];
+        for(int i = 0; i < resultPath.length - 1; i++) {
+            resultPath[i] = nodes[citiesFromAndTo[1]].getShortestPath().get(i);
+        }
+        resultPath[resultPath.length - 1] = nodes[citiesFromAndTo[1]];
+
+        return resultPath;
+    }
 
 }
